167bb4f759d46198b5af35f6d8f072259e83cc67
BenEsquivelMusic@gmail.com <BenEsquivelMusic@gmail.com>
2025-02-17 21:02:20 -0500

    Skeleton application added.

---
+++ modules/application/src/main/java/com/bem/Application.java
@@ -0,0 +1,13 @@
+package com.bem;
+
+public final class Application {
+
+    private Application() {
+        /* Main application class  */
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
