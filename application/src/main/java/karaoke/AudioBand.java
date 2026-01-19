ee6f14c81f5dee70dd445d145ffcdcfa903055de
BenEsquivelMusic@gmail.com <BenEsquivelMusic@gmail.com>
2026-01-19 11:13:19 -0500

    Update AudioBand class to have better labeling.

--- application/src/main/java/karaoke/AudioBand.java
+++ application/src/main/java/karaoke/AudioBand.java
@@ -1,20 +1,23 @@
 package karaoke;
 
+import java.util.List;
 import java.util.Objects;
 
 public final class AudioBand {
 
-    private static final String PHASE_LABEL_PREFIX = "Band ";
+    private static final List<String> LABELS = List.of("60Hz", "170Hz", "310Hz", "600Hz",
+            "1kHz", "3kHz", "6kHz", "12kHz", "14kHz", "16kHz");
 
     public final int band;
 
     public AudioBand(int band) {
         this.band = band;
+        validate();
     }
 
     @Override
     public String toString() {
-        return PHASE_LABEL_PREFIX + band;
+        return LABELS.get(band - 1);
     }
 
     @Override
@@ -31,6 +34,12 @@ public final class AudioBand {
             return this.band == otherAudioBand.band;
         }
         return false;
+    }
+
+    private void validate() {
+        if (band > LABELS.size()) {
+            throw new IllegalArgumentException("Invalid band number provided: " + band);
+        }
     }
 
 }
